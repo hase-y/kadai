6a30de7f02f3ad9671b8c156e0dacf089b8b03c4
hase <xiongwuzhangguchuan@gmail.com>
2022-02-15 10:18:54 +0000

    PHP/Laravel 09 kadai

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+//課題３
+//http://XXXXXX.jp/XXXというアクセスが来たら
+//→AAAControllerのbbbというアクションに渡す
+//Route::get('XXX','AAAController@bbb');
+
+//課題４
+Route::group(['prefix'=> 'admin'],function(){
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+});
